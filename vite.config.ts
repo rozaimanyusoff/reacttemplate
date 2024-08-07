2ce5fded2168f0c6817fd25258de080db4edddec
rozaimanyusoff <rozaimanyusoff@gmail.com>
2024-08-07 10:20:42 +0800

    Add update from macbook

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,19 @@ export default defineConfig({
     plugins: [
         react(),
     ],
+    base: '/reacttemplate/',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
+        },
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].[hash].js',
+                chunkFileNames: 'assets/[name].[hash].js',
+                assetFileNames: 'assets/[name].[hash].[ext]',
+            },
         },
     },
 });
