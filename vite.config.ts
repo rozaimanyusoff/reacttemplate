5cb12a6ce5e1122672fbb59207943283b687f2d5
rozaimanyusoff <rozaimanyusoff@gmail.com>
2024-08-10 16:41:57 +0800

    finale2

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,7 @@ export default defineConfig({
     plugins: [
         react(),
     ],
-<<<<<<< HEAD
     //base: '/reacttemplate',
-=======
-    //base: process.env.NODE_ENV === 'production' ? '/reacttemplate/' : '/',
->>>>>>> 67773e781de8e6f825be2448078d66bfeb6f9d5a
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
