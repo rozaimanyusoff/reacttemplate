1eb03949f4e6129f65132a9a858ae0829c657142
rozaimanyusoff <88320271+rozaimanyusoff@users.noreply.github.com>
2024-08-10 23:33:59 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
     plugins: [
         react(),
     ],
-    base: '/reacttemplate/',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
